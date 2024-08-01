9e5710ecffae98c1bcd569bcbba87c228dc87e83
Anor Bertran <anor.bertran@fake.com>
2024-08-01 22:46:46 +0400

    Beautify test game set

--- src/GameSetLoader.cpp
+++ src/GameSetLoader.cpp
@@ -7,11 +7,12 @@ GameSet GameSetLoader::load()
         GameSet::Round round;
         for (int j = 1; j < 7; ++j) {
             GameSet::Category category;
-            category.name = L"Cat " + std::to_wstring(j + 10 * k);
+            category.name = L"Category " + std::to_wstring(j + 10 * k);
             for (int i = 1; i < 6; ++i) {
                 GameSet::Card c;
                 c.price = (k + 1) * i * 100;
-                c.question = L"Question from " + category.name + L" question " + std::to_wstring(c.price) + L"?";
+                c.question = L"Amazing question from cool " + category.name + L" with price " + std::to_wstring(c.price)
+                    + L"?";
                 category.cards.push_back(c);
             }
             round.categories.push_back(category);
