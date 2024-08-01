f7011924935943ac41edee28795b589a2fa07b1b
Anor Bertran <anor.bertran@fake.com>
2024-08-01 22:46:47 +0400

    Fix PlayerCard clickability

--- src/widgets/PlayerCard.cpp
+++ src/widgets/PlayerCard.cpp
@@ -6,15 +6,18 @@ PlayerCard::PlayerCard(const Player &player)
 {
     auto *nicknameLabel = new TextLabel(player.nickname);
     nicknameLabel->setId(L"NicknameLabel");
+    nicknameLabel->setMouseTransparent(true);
 
     m_scoreLabel = new TextLabel();
     m_scoreLabel->setId(L"ScoreLabel");
+    m_scoreLabel->setMouseTransparent(true);
     setScore(player.score);
 
     auto *gridLayout = new GridLayout(5, 1);
     gridLayout->setId(L"PlayerGridLayout");
     gridLayout->adjustWidgetSize(true);
     gridLayout->setParent(this);
+    gridLayout->setMouseTransparent(true);
 
     gridLayout->addWidget(0, 0, nicknameLabel);
     gridLayout->addWidget(4, 0, m_scoreLabel);
